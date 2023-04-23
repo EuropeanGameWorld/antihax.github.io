be9a31d807c0cdabcb06c164a9cee4675a365c60
Taelyn <38536376+Taelyn@users.noreply.github.com>
2023-04-23 10:35:29 +0300

    Add EGW Server Atlas Map

--- json-server/config.js
+++ json-server/config.js
@@ -1,4 +1,5 @@
 {
+	"AtlasMapServer": false,
 	"GPSBounds": {
 		"max": [
 			100,
@@ -11,9 +12,14 @@
 	},
 	"GridOffset": 23333.333333333332,
 	"GridSize": 1400000,
+	"ItemLink": true,
+	"KofiLink": true,
 	"NodesPerAxis": 30,
-	"ServersX": 5,
-	"ServersY": 5,
+	"PathFinder": true,
+	"PinTool": true,
+	"ServersX": 9,
+	"ServersY": 9,
+	"WarehouseTool": false,
 	"XRange": 200,
 	"XScale": 1,
 	"YRange": 200,
